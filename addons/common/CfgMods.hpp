2a5a5083fc4f8cfabb2f578e685946eb8feadeee
Ramsey <thunt501stramsey@gmail.com>
2026-06-25 10:26:46 +0100

    common

---
+++ addons/common/CfgMods.hpp
@@ -0,0 +1,14 @@
+class CfgMods {
+	class cadcore{
+		dir = "@Cadian Core";
+		name = "Cadian Core";
+
+		author = "Rowdy Rodent Industries";
+
+		hidePicture = 0;
+		hideName = 1;
+
+		description = "Cadian Core";
+		overview = "Cadian Core";
+	};
+};
