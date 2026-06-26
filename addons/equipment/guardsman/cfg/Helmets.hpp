fbfd2bc81e8324e97f0b365b75c9170316d96f79
Ramsey <thunt501stramsey@gmail.com>
2026-06-26 16:15:42 +0100

    ws

--- addons/equipment/guardsman/cfg/Helmets.hpp
+++ addons/equipment/guardsman/cfg/Helmets.hpp
@@ -2,10 +2,17 @@ class GHELMET(base): FIG_CadianHelm {
     scope = 2;
     scopeArsenal = 2;
     author = AUTHOR;
-    displayName = "[BAW] Battledroid Interface";
+    displayName = "[41st] Guardsman Helmet";
     hiddenSelectionsTextures[] = {QPATHTOF(data\helmet\Helmet_co.paa)};
     class ItemInfo: ItemInfo {
         HELMET_PROTECTION;
     };
     HELMET_ACE_HEARING;
+};
+class GHELMET(ws): GHELMET(base){
+    scope = 2;
+    scopeArsenal = 2;
+    author = AUTHOR;
+    displayName = "[41st] Whiteshield Helmet";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\helmet\Helmet_WS_co.paa)};
 };
